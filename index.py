db837c0902d64784951f91a18aa220d84b3cf441
EmmzyTech <emmzytech@gmail.com>
2022-07-02 07:13:08 -0400

    Updated Extensions and Directory inputs

--- index.py
+++ index.py
@@ -2,9 +2,27 @@ import os
 import pathlib
 import shutil
 
-fileExtension = input("Input file extension: ").lower()
+# fileExtension = input("Input file extension: ").lower()
 
-dir = str(pathlib.Path.home() / "Downloads/CapaSorter") #Path to my test directory
+print("***************" + "HOME PATH ENTRIES "+"  ***************")
+parent_dir_name = input("enter name of parent directory(Downloads,Desktop,Documents ...): ")
+
+print("***************" + "SUB PATH ENTRIES "+"  ***************")
+dir_name = input("Enter directory name: e.g work/designs ")
+
+dir = str(pathlib.Path.home() / (parent_dir_name + "/" +dir_name)) #Path to my test directory
+
+ext = []
+
+
+def getActiveExtensions(dir):
+  files = os.listdir(dir)
+  for items in files:
+      index = items.rfind(".")
+      if items[index:] not in ext:
+        ext.append(items[index:])
+      
+getActiveExtensions(dir)
 
 def CreateFolder(directory):
   try:
@@ -12,17 +30,22 @@ def CreateFolder(directory):
       new_path = dir + "/" + directory
       os.mkdir(new_path)
   except FileExistsError:
-      pass
+    pass
+#Move all files 
 def MoveFiles(filename, destination):
-  desc = dir + "/" + destination
-  shutil.move(dir +"/"+ filename, desc)
-  pass
+  try:
+    desc = dir + "/" + destination
+    shutil.move(dir +"/"+ filename, desc)
+  except FileNotFoundError:
+    pass
+  
 # CreateFolder("Working")   
 for item in os.listdir(dir):
-  if item.endswith(f".{fileExtension}"):
-    fileType = fileExtension.title()
-    CreateFolder(fileType + " Files")
-    MoveFiles(item, fileType + " Files")
+  for end in ext:
+    if item.endswith(end):
+      fileType = end.title().replace('.', '')
+      CreateFolder(fileType + " Files")
+      MoveFiles(item, fileType + " Files")
+print("*********************************\n*\t Sorting Successful: Check Directory to Confirm *********************************")
 
 
-# print(dir)

c6ab258f051866109020e4743f88a2c2b47614de
callistus406 <callistus406@gmail.com>
2022-07-08 18:44:05 +0100

    did some code refactoring and error handling

--- index.py
+++ index.py
@@ -1,85 +1,61 @@
 import os
+import pathlib
 import shutil
+
+# fileExtension = input("Input file extension: ").lower()
 ext = []
-def get_path(name):
-    # accepts two arguments, parent directory(system defined directories) and child directory(arbitrary name, provided the dir is already created )
-    print("")
-    print("***************  "+name+" PATH ENTRIES "+"  ***************")
+# get directory path from the user
+def getPath():
+    print("\n***************" + "HOME PATH ENTRY "+"  ***************")
     parent_dir_name = input("enter name of parent directory(Downloads,Desktop,Documents ...): ")
-    # parent_dir_name = "Desktop"
-    dir_name = input("Enter directory name: ")
-    # dir_name = location
-    print(dir_name) 
-    if dir_name != None :
-        dir = parent_dir_name +"/"+dir_name
-        env = os.environ.get("HOME")
-        path = os.path.join(env,dir)
-        # print("\n*****************************************************************"+"\n")
-    else:
-        path = os.environ.get("HOME")
-        # print("\n*****************************************************************"+"\n")
-    return path
 
-src = get_path("SOURCE")
-
-
-dest = get_path("DESTINATION")
-
-
-def validate_path():
-
-    # validates the provided path and stores the extensions in ext list
-    verify = os.path.exists(src)
-
-    if verify:
-        files = os.listdir(src)
-        for file in files:
-            index = file.find(".")
-            ext.append(file[index+1:]) 
-    else:
-        return "not a valid path" 
-  
-
-
-def create_folder():
-    validate_path()
-    for name in ext:
-                # print(name)/
-        if name in os.listdir(dest):
-                # print(f"{name} folder already created")
-            pass
-        else:
-            os.mkdir(dest+"/"+name)
+    print("\n***************" + "SUB PATH ENTRY "+"  ***************")
+    dir_name = input("Enter directory name: e.g work/designs: ")
     
+    return str(pathlib.Path.home() / (parent_dir_name.title() + "/" +dir_name)) #Path to my test directory
+
+dir = getPath()
+
+def getActiveExtensions(dir):
+      try:
+          files = os.listdir(dir)
+          for items in files:
+              index = items.rfind(".")
+              if items[index:] not in ext:
+                ext.append(items[index:])
+      except FileNotFoundError:
+         print('The provided path "'+dir+'" Does not exist')
+      
+getActiveExtensions(dir)
+
+def CreateFolder(directory):
+  try:
+    if not os.path.exists(directory):
+      new_path = dir + "/" + directory
+      os.mkdir(new_path)
+  except FileExistsError:
+    pass
+#Move all files 
+def MoveFiles(filename, destination):
+  try:
+    desc = dir + "/" + destination
+    shutil.move(dir +"/"+ filename, desc)
+  except FileNotFoundError:
+    pass
   
-
-def move_files():
-    try:
-        # creates folders base on the file ext received
-        create_folder()
-        # reads the content of the source directory
-        list = os.listdir(src)
-        if list:  
-            # loops through the content of list and performs equality check
-            for i in range(len(list)) :
-                for j in range(len(list)):
-                    index = list[i].find(".")
-                    ans =list[i][index+1:]
-                    if ans == ext[j]:
-        # moves all files found in th src to dest to their respective folders
-                        if list[i] in os.listdir(dest+"/"+ext[j]):
-                            pass
-                        else:
-                            shutil.move(src+"/"+list[i],dest+"/"+ext[j])
-                        
-            print("*********************************\n*\tACTION SUCCESSFUL\t*\n*********************************")
-
-        else:
-            print("No file found")
-    except FileNotFoundError:
-        print("No such file or directory")
+# CreateFolder("Working")   
+def CapaSorter():
+  try:
+    for item in os.listdir(dir):
+      for end in ext:
+        if item.endswith(end):
+          fileType = end.title().replace('.', '')
+          CreateFolder(fileType + " Files")
+          MoveFiles(item, fileType + " Files")
+    print("**************************************************\n* Sorting Successful: Check Directory to Confirm *\n**************************************************")
+  except FileNotFoundError:
+    pass
 
 
-    
-
-move_files()
+CapaSorter()
+  
